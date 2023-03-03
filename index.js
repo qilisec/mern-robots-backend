1232d84899ffd6162ca0d20653b6e2633c8115d3
linanthonyfirst <linanthony.main@gmail.com>
2023-03-02 21:27:01 -0500

    Added HTTPS Configuration

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const os = require("os")
+const https = require("https")
+const fs = require("fs")
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -7,6 +10,8 @@ const robotsRouter = require('./routes/robotsRouter');
 
 const app = express();
 const apiPort = 3000;
+console.log(`os type is ${os.type()}, platform is ${os.platform()}`)
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -17,10 +22,20 @@ db.once('open', () => {
 });
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+https
+  .createServer(
+    {
+      key: fs.readFileSync("./keys/cert-key.pem"),
+      cert: fs.readFileSync("./keys/cert.pem"),
+    }, app)
+  .listen(apiPort, () => {
+    console.log(`Server running on port ${apiPort}`)
+})
+
 app.get('/', (req, res) => {
   res.send('Robot Backend Home');
 });
 
 app.use('/api', robotsRouter);
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
+// app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
